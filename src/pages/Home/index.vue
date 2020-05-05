3da01c323a0ac89277cb7af6f58387b0dec70457
Di Jiayi <2328444141@qq.com>
2020-05-05 12:04:54 +0800

    feat:Home静态组件完成

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>home</div>
+  <div>
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
+  </div>
 </template>
 
 <script>
+import ListContainer from "./ListContainer/ListContainer";
+import TodayRecommend from "./TodayRecommend/TodayRecommend";
+import Rank from "./Rank/Rank";
+import Like from "./Like/Like";
+import Floor from "./Floor/Floor";
+import Brand from "./Brand/Brand";
 export default {
   name: "Home",
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
 };
 </script>
 
