c633f56b0657484cb750b91b937290f062cca7dc
Di Jiayi <2328444141@qq.com>
2020-05-09 16:06:08 +0800

    feat:Search动态显示数据完成和解决不进行再次搜索的bug

---
+++ src/store/modules/search.js
@@ -0,0 +1,27 @@
+import { reqProductList } from "@/api";
+
+const state = {
+  productList: {},
+};
+const mutations = {
+  RECEIVE_PRODUCT_LIST(state, productList) {
+    state.productList = productList;
+  },
+};
+const actions = {
+  async getProductList({ commit }, searchParams) {
+    const result = await reqProductList(searchParams);
+    if (result.code === 200) {
+      const productList = result.data;
+      commit("RECEIVE_PRODUCT_LIST", productList);
+    }
+  },
+};
+const getters = {};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
